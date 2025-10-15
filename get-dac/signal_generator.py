6d1ad3e9d7ad5ff893a50c2a7697efce72cf7f0a
Volkov Ilya <volkov.ilia@phystech.edu>
2025-10-15 23:11:34 +0300

    добавил новую функцию в генератор сигнала

--- get-dac/signal_generator.py
+++ get-dac/signal_generator.py
@@ -3,7 +3,6 @@ import numpy as np
 import math
 
 def get_sin_wave_amplitude(freq, time):
-##   return (np.sin(2 * np.pi * freq * time) + 1) / 2
     sinus = math.sin(2*np.pi*freq*time)
     return (sinus+1)/2
 
@@ -12,3 +11,8 @@ def wait_for_sampling_period(sampling_freq):
 
 def tri_gen(freq, time):
     return ((-2.0 / np.pi) * math.asin(math.sin(2*np.pi*freq*time))+1)/2
+
+def abscos(freq, time):
+    return abs(((math.cos(2*np.pi*freq*time))+1)/2)
+
+    
